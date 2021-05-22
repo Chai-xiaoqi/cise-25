f99dc392fe9da4e5f397ecc084f9f85d5707fabe
Chai-xiaoqi <a1057492951@gmail.com>
2021-05-22 14:05:54 +1200

    create admin and user page

--- cise-25-client/src/App.js
+++ cise-25-client/src/App.js
@@ -4,18 +4,27 @@ import './App.css';
 
 import CreateBook from './components/CreateBook';
 import ShowBookList from './components/ShowBookList';
+import ShowBookList_admin from './components/ShowBookList_admin';
 import ShowBookDetails from './components/ShowBookDetails';
+import ShowBookDetails_admin from './components/ShowBookDetails_admin';
 import UpdateBookInfo from './components/UpdateBookInfo';
+import SearchBook from './components/SearchBook';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path='/' component={ShowBookList} />
+
+          <Route path='/show-books' component={ShowBookList} />
+          <Route path='/show-books_admin' component={ShowBookList_admin} />
           <Route path='/create-book' component={CreateBook} />
           <Route path='/edit-book/:id' component={UpdateBookInfo} />
           <Route path='/show-book/:id' component={ShowBookDetails} />
+          <Route path='/show-book_admin/:id' component={ShowBookDetails_admin} />
+          <Route path='/search-book/:id' component={SearchBook} />
+       
+          
         </div>
       </Router>
     );
--- routes/api/books.js
+++ routes/api/books.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // Load Book model
 const Book = require('../../models/Book');
-const Article = require('../../models/Article');
+//const Article = require('../../models/Article');
 
 // @route GET api/books/test
 // @description tests books route
